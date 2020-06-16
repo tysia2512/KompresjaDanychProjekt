ab2f6241ccc5f8adfa645031112f3da15830d517
tysia2512 <tysia2512@gmail.com>
2020-06-16 19:14:28 +0200

    Ostatnie poprawki

--- run_test.cpp
+++ run_test.cpp
@@ -1,18 +1,33 @@
 #include "test.cpp"
 
 const std::vector<Test> all_tests{
-    Test("king_lear", 5), 
-    Test("king_lear", 3, true), 
-    Test("king_lear_lc", 6), 
-    Test("king_lear_lc", 3, true), 
-    Test("strona1", 5),
-    Test("strona1", 2, true),
-    Test("download.jpeg", 5),
-    Test("download.jpeg", 2, true),
+    // Test("king_lear", 5), 
+    // Test("king_lear", 3, true), 
+    // Test("king_lear_lc", 6), 
+    // Test("king_lear_lc", 4, true), 
+    // Test("strona1", 5),
+    // Test("strona1", 2, true),
+    // Test("strona1", 2, true, true),
+    Test("pies.jpeg", 5),
+    Test("pies.jpeg", 5, true, true),
+    Test("pies.jpeg", 3, true),
+};
+
+const std::vector<Test> chart1{
+    Test("pies.jpeg", 6),
+    Test("pies.jpeg", 6, true, true),
+    Test("pies.jpeg", 3, true),
+};
+
+const std::vector<Test> tests{
+    Test("king_lear", 10, true, true),
+    Test("king_lear_lc", 10, true, true),
+    Test("pies.jpeg", 10, true, true),
+    Test("skos", 10, true, true),
 };
 
 void run_all_tests() {
-    for (const Test &test : all_tests) {
+    for (const Test &test : tests) {
        test.run();
     }
 }
